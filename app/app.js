21a8ae7cf495de3ddec6bf7ac307dcdb12bf5a03
Kevin Lin <klin50@stuy.edu>
2025-05-09 10:58:02 -0400

    remade app.js added the song and added requirements

--- app/app.js
+++ app/app.js
@@ -1,11 +1,24 @@
-const { createServer } = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/',(req,res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+function getBeats(){
+  var song = 'Mortals.mp3';
+
+  const spawn = require("child_process").spawn;
+  const pythonProcess = spawn('python',["beats.py", song]);
+
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+}
+
+getBeats();
---
+++ app/beats.py
@@ -0,0 +1,14 @@
+import librosa
+import sys
+
+song = sys.argv[1]
+
+y, sr = librosa.load(song)
+
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+
+for beat in beat_times:
+    print(int(beat))
+sys.stdout.flush()
