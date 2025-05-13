60ca3c7637fc9cda9ee45cf8febe6f1db2fb1f78
Danny Mok <144475495+Imokudan@users.noreply.github.com>
2025-05-13 00:42:47 -0400

    added temporary code to app

--- app/app.js
+++ app/app.js
@@ -1,3 +1,16 @@
+//temporary code
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+
+/*
 const express = require('express');
 const app = express();
 const port = 3000
@@ -22,3 +35,4 @@ function getBeats(){
 }
 
 getBeats();
+*/
