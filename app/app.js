9a16d4948fee73cb26ba2c68869624c5e5b7e5f7
Danny Mok <144475495+Imokudan@users.noreply.github.com>
2025-05-13 00:43:20 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,13 +1,17 @@
 //temporary code
-import { createServer } from 'node:http';
+const http = require('http');
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!\n');
 });
 
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 /*
