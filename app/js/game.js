48104e03b656c9c08670e6342ed6e0bf8b12be80
Kevin Lin <klin50@stuy.edu>
2025-05-20 10:49:31 -0400

    added song and beats

--- app/js/game.js
+++ app/js/game.js
@@ -1,14 +1,14 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 var song = document.getElementById("song");
-let beats = [];
+var beats = [];
 
 function getBeats(){
   fetch('/api/beats')
     .then(response => response.json())
     .then(data => {
       console.log('Beats data:', data);
-      beats = data;
+      beats = setLanes(data);
     })
     .catch(err => {
       console.error('Error fetching beats:', err);
@@ -37,15 +37,32 @@ function drawLanes(){
   }
 }
 
+function beat(lane,beat){
+  this.lane = lane;
+  this.beat = beat;
+}
+
+function setLanes(arr){
+  let laneNum = 0;
+  let array = [];
+  for (i = 0; i < arr.length; i ++){
+    if(laneNum == 4){
+      laneNum = 0;
+    }
+    array[i] = new beat(laneNum,arr[i]);
+    laneNum ++;
+  }
+  return array;
+}
+
 function drawRects(){
+  ctx.beginPath();
   for(i = 0; i < beats.length; i++){
-    if(song.currentTime-beats[i]<= 3 && song.currentTime-beats[i]>-1){
-      console.log(beats[i]);
-      ctx.beginPath();
-      ctx.rect(0,canvas.height*(1-(beats[i]-song.currentTime)),canvas.width/4,canvas.height/8);
-      ctx.fill();
+    if(beats[i].beat-song.currentTime<= 5 && beats[i].beat-song.currentTime>-1){
+      ctx.rect(beats[i].lane*canvas.width/4,canvas.height*(1-(beats[i].beat-song.currentTime)/3),canvas.width/4,canvas.height/8);
     }
   }
+  ctx.fill();
 }
 
 function animate(){
