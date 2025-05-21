721c10a7d380055fcd0c0789f049455f411c696e
Danny <dmok50@stuy.edu>
2025-05-21 10:40:16 -0400

    updatede db

--- app/js/db.js
+++ app/js/db.js
@@ -1,21 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
-const create = function () {
+const create = async () => {
   db.serialize(() => {
     db.run("DROP TABLE IF EXISTS users");
-    db.run('CREATE TABLE users (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              username TEXT UNIQUE NOT NULL,
-              password TEXT NOT NULL
-              )
-          ');
-    db.run('CREATE TABLE IF NOT EXISTS scores (
-              songs TEXT NOT NULL,
-              highscore INTEGER DEFAULT 0,
-              id INTEGER NOT NULL
-              )
-          ');
+    db.run('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL,password TEXT NOT NULL)');
+    db.run('CREATE TABLE IF NOT EXISTS scores (songs TEXT NOT NULL, highscore INTEGER DEFAULT 0, id INTEGER NOT NULL)');
     });
   db.close();
 }
