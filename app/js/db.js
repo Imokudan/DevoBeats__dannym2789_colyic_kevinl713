876566677f5b2448551291572d7ae823330e5abf
Danny <dmok50@stuy.edu>
2025-05-22 11:01:49 -0400

    update db

--- app/js/db.js
+++ app/js/db.js
@@ -3,52 +3,57 @@ const db = new sqlite3.Database(':memory:');
 
 const create = async () => {
   db.serialize(() => {
-    db.run("DROP TABLE IF EXISTS users");
-    db.run('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL,password TEXT NOT NULL)');
-    db.run('CREATE TABLE IF NOT EXISTS scores (songs TEXT NOT NULL, highscore INTEGER DEFAULT 0, id INTEGER NOT NULL)');
+    db.run("DROP TABLE IF EXISTS users;");
+    db.run("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL);");
+    db.run("CREATE TABLE IF NOT EXISTS scores (songs TEXT NOT NULL, highscore INTEGER DEFAULT 0, id INTEGER NOT NULL);");
     });
+    console.log("Tables ready");
   db.close();
 }
 
-const createUser = () => {
+const createUser = (name, pass) => {
+  let state = false;
   db.serialize(){
+    db.all("SELECT id from users where username = ?", name, function(err, rows){
+      if (rows.length == 0){
+        db.run("INSERT INTO users (username, password) VALUES (?, ?);", (name, pass));
+        console.log("Successfully added user: " + name);
+        state = true;
+      }
+      else{
+        console.log("Username " + name + " already exists!");
+      }
+    });
 
   };
   db.close()
+  return state;
 }
 
-const getUser = function() {
-  let data;
+const getUser = (user) => {
+  let data = "";
   db.serialize(){
-    db.get('SELECT * FROM users WHERE id = ?', username, function(err, row){
-      console.log("Getting user: " + username);
-      data = row["username"];
-      console.log("Username " + username + " obtained");
+    db.get("SELECT * FROM users WHERE username = ?", user, function(err, row){
+      console.log("Getting user: " + user);
+      data = row.username;
+      console.log("Username " + user + " obtained");
     });
   };
   db.close()
   return data;
 }
 
-const createPass = () => {
+const getPass = (user) => {
   db.serialize(){
-
+    let data = "";
+    db.get("SELECT * FROM users WHERE username = ?", user, function(err, row){
+      console.log("Getting password for user: " + user);
+      data = row.password;
+      console.log("Password obtained for user: " + user);
+    });
   };
   db.close()
-}
-
-const getPass = () => {
-  db.serialize(){
-
-  };
-  db.close()
-}
-
-const printdB = () => {
-  db.serialize(){
-
-  };
-  db.close()
+  return data;
 }
 
 const getUserScores = (username) => {
@@ -65,4 +70,13 @@ const getUserScores = (username) => {
   };
   db.close()
   return userscores;
+}
+
+const setUserScore = (username, song, score) => {
+  let state = false;
+  db.serialize(){
+
+  }
+  db.close;
+  return state;
 }
