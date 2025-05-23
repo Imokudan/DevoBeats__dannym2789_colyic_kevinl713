2ad4411524e6bbcd4e8e36a1a58672bddb00507d
Danny <dmok50@stuy.edu>
2025-05-23 11:01:25 -0400

    updated db

--- app/js/db.js
+++ app/js/db.js
@@ -13,7 +13,7 @@ const create = async () => {
 
 const createUser = (name, pass) => {
   let state = false;
-  db.serialize(){
+  db.serialize(() => {
     db.all("SELECT id from users where username = ?", name, function(err, rows){
       if (rows.length == 0){
         db.run("INSERT INTO users (username, password) VALUES (?, ?);", (name, pass));
@@ -24,41 +24,40 @@ const createUser = (name, pass) => {
         console.log("Username " + name + " already exists!");
       }
     });
-
-  };
+  });
   db.close()
   return state;
 }
 
 const getUser = (user) => {
   let data = "";
-  db.serialize(){
+  db.serialize(() => {
     db.get("SELECT * FROM users WHERE username = ?", user, function(err, row){
       console.log("Getting user: " + user);
       data = row.username;
       console.log("Username " + user + " obtained");
     });
-  };
+  });
   db.close()
   return data;
 }
 
 const getPass = (user) => {
-  db.serialize(){
+  db.serialize(() => {
     let data = "";
     db.get("SELECT * FROM users WHERE username = ?", user, function(err, row){
       console.log("Getting password for user: " + user);
       data = row.password;
       console.log("Password obtained for user: " + user);
     });
-  };
+  });
   db.close()
   return data;
 }
 
-const getUserScores = (username) => {
+const getUserAllScores = (username) => {
   let userscores = [];
-  db.serialize(){
+  db.serialize(() => {
     db.all('SELECT * FROM scores WHERE id = ?', username, function(err, rows){
       console.log("Getting high scores of user: " + username);
       for (let i = 0; i < userscores.length; i++){
@@ -67,16 +66,28 @@ const getUserScores = (username) => {
       console.log("User high scores obtained");
       console.log("User: " + username + "'s scores: " + userscores)
     });
-  };
+  });
   db.close()
   return userscores;
 }
 
+const getUserSongScores = (username, song) => {
+  let data = [];
+  if (){
+    data.push(song);
+    db.serialize(() => {
+      db.run()
+    });
+    db.close;
+  }
+  return data;
+}
+
 const setUserScore = (username, song, score) => {
   let state = false;
-  db.serialize(){
+  db.serialize(() => {
 
-  }
+  });
   db.close;
   return state;
 }
