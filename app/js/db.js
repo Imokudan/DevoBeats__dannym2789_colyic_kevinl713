22c0367b90aeeda679d85a7bf57f4bdd7b2a1ce8
Danny <dmok50@stuy.edu>
2025-05-28 11:01:45 -0400

    updated db

--- app/js/db.js
+++ app/js/db.js
@@ -1,7 +1,29 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
-const create = async () => {
+const fetchAll = async (sql, params) => {
+  return new Promise((pass, fail) => {
+    db.serialize(() => {
+      db.all(sql, params, (err, rows) => {
+        if (err) {fail(err);}
+        pass(rows);
+      });
+    });
+  });
+}
+
+const fetchFirst = async (sql, params) => {
+  return new Promise((pass, fail) => {
+    db.serialize(() => {
+      db.get(sql, params, (err, row) => {
+        if (err) {fail(err);}
+        pass(row);
+      });
+    });
+  });
+}
+
+const createTables = async () => {
   db.serialize(() => {
     db.run("DROP TABLE IF EXISTS users;");
     db.run("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL);");
@@ -11,25 +33,22 @@ const create = async () => {
   db.close();
 }
 
-const createUser = (name, pass) => {
+const createUser = async (name, pass) => {
   let state = false;
   db.serialize(() => {
-    db.all("SELECT id from users where username = ?", name, function(err, rows){
-      if (rows.length == 0){
-        db.run("INSERT INTO users (username, password) VALUES (?, ?);", (name, pass));
-        console.log("Successfully added user: " + name);
-        state = true;
-      }
-      else{
-        console.log("Username " + name + " already exists!");
-      }
+    let rowarray = fetchAll("SELECT id from users where username = ?", name);
+    if (rowarray.length == 0){
+      db.run("INSERT INTO users (username, password) VALUES (?, ?);", (name, pass));
+      console.log("Successfully added user: " + name);
+      state = true;
+    }
+    else {console.log("Username " + name + " already exists!");}
     });
-  });
   db.close();
   return state;
 }
 
-const getUser = (user) => {
+const getUser = async (user) => {
   let data = "";
   db.serialize(() => {
     db.get("SELECT * FROM users WHERE username = ?", user, function(err, row){
@@ -42,7 +61,7 @@ const getUser = (user) => {
   return data;
 }
 
-const getPass = (user) => {
+const getPass = async (user) => {
   db.serialize(() => {
     let data = "";
     db.get("SELECT * FROM users WHERE username = ?", user, function(err, row){
@@ -52,12 +71,10 @@ const getPass = (user) => {
     });
   });
   db.close();
-  return data;for (let i = 0; i < userscores.length; i++){
-        userscores.push(rows[i]);
-
+  return data;
 }
 
-const getUserAllScores = (usernameid) => {
+const getUserAllScores = async (usernameid) => {
   let userscores = [];
   db.serialize(() => {
     db.all('SELECT * FROM scores WHERE id = ?', usernameid, function(err, rows){
@@ -73,10 +90,10 @@ const getUserAllScores = (usernameid) => {
   return userscores;
 }
 
-const getUserSongScores = (usernameid, song) => {
+const getUserSongScores = async (usernameid, song) => {
   let data = [];
   db.serialize(() => {
-    if ["Sample1","Sample2"].includes(song);{
+    if (["Sample1","Sample2"].includes(song)){
         console.log("Getting song: " + song + "for user: " + userid);
         db.all('SELECT * FROM scores WHERE id = ? AND songs = ?', (usernameid, song), function(err, rows){
           for (let i = 0; i < rows.length; i++){
@@ -90,11 +107,14 @@ const getUserSongScores = (usernameid, song) => {
   return data;
 }
 
-const setUserScore = (usernameid, song, score) => {
+const setUserScore = async (usernameid, song, score) => {
   let state = false;
   db.serialize(() => {
-    db.run("INESERT INTO scores (score)");
+    db.run("INESERT INTO scores (id, songs, scores) VALUES (?, ?, ?);", (usernameid, song, score));
   });
-  db.close;
+  db.close();
   return state;
 }
+
+createTables();
+createUser("danny", "password");
